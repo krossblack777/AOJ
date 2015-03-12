14c53dfb573a89dd50a3dfba6663c62eb1e2d2b9
krossblack777 <krossprogram@gmail.com>
2015-03-12 22:16:10 +0900

    Fix

--- Introduction/Volume100/10008_AB_Problem.rb
+++ Introduction/Volume100/10008_AB_Problem.rb
@@ -1,9 +1,9 @@
 a, b = gets.split.map &:to_f
 printf("%d %d %.5f\n",a/b,a%b,a/b)
 
-printf ()のように空白入れるとエラー
 =begin
 
+printf ()のように空白入れるとエラー
 a, b = gets.split.map(&:to_f)
 printf "%i %i %f\n", a/b, a%b, a/b
 printfの使い方に注意
